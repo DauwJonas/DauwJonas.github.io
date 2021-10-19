37a57f2e87747a28a43de5c722e28936982fe639
Dauw_Jonas <43506590+DauwJonas@users.noreply.github.com>
2021-10-19 11:23:12 +0200

    Update

--- Library/js/Make-ImgText-ListItem.js
+++ Library/js/Make-ImgText-ListItem.js
@@ -7,13 +7,17 @@
         const listItemEl = allListItemsEl[i];
         //let attributeNames = { h4: "data-h4", a: "data-a", img: "data-img", p: "data-p" };
         //const data = { h4: listItemEl.getAttribute(attributeNames.h4), a: listItemEl.getAttribute(attributeNames.a), img: listItemEl.getAttribute(attributeNames.img), p: listItemEl.getAttribute(attributeNames.p) };
-        let attributeNames = ["data-h4", "data-a", "data-img", "data-p"];
-        const data = { h4: listItemEl.getAttribute(attributeNames[0]), a: listItemEl.getAttribute(attributeNames[1]), img: listItemEl.getAttribute(attributeNames[2]), p: listItemEl.getAttribute(attributeNames[3]) };
+        let attributeNames = ["data-h4", "data-a", "data-img", "data-p", "data-a_target"];
+        let data = { h4: listItemEl.getAttribute(attributeNames[0]), a: listItemEl.getAttribute(attributeNames[1]), img: listItemEl.getAttribute(attributeNames[2]), p: listItemEl.getAttribute(attributeNames[3]), a_target: listItemEl.getAttribute(attributeNames[4]) };
+        //Data correction
+        if (data.a_target == null || data.a_target == undefined) {
+            data.a_target = "";
+        }
         //Remove date from ListItem
         for (let k = 0; k < attributeNames.length; k++) {
             const attributeName = attributeNames[k];
             listItemEl.removeAttribute(attributeName);
         }
-        listItemEl.innerHTML = `<a href="${data.a}"target="_blank"class="img"><img src="${data.img}"></a><div><h4><a href="${data.a}"target="_blank">${data.h4}</a></h4><p>${data.p}</p><a href="${data.a}"target="_blank">${data.h4}</a></div>`;
+        listItemEl.innerHTML = `<a href="${data.a}"target="${data.a_target}"class="img"><img src="${data.img}"></a><div><h4><a href="${data.a}"target="${data.a_target}">${data.h4}</a></h4><p>${data.p}</p><a href="${data.a}"target="${data.a_target}">${data.h4}</a></div>`;
     }
 })();
