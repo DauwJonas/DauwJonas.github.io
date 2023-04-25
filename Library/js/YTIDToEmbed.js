fb1fa0a713de2038347420747f5dd8cf3c0c15f4
Dauw_Jonas <43506590+DauwJonas@users.noreply.github.com>
2023-04-25 12:42:53 +0200

    Update to Sass & Factorio & YTVids
    * Rm NSFW warning on vids.
    * Vid fix
    * Delete wiki Pics (got removed)
    * Add Tutorial Opinion

--- Library/js/YTIDToEmbed.js
+++ Library/js/YTIDToEmbed.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     let picToEmbed = document.getElementsByClassName("PicToEmbed");
@@ -8,11 +8,12 @@
         nEl.classList.add("VideoImg");
         nEl.src = "https://i.ytimg.com/vi/" + iEl.getAttribute("data-YouTubeVideoID") + "/sddefault.jpg";
         iEl.appendChild(nEl);
-        iEl.children[0].addEventListener("click", function(e) {
-            let iEl = e.path[1];
+        iEl.children[0].addEventListener("click", function (e) {
+            console.log(e, "e", e.target);
+            let iEl = e.target.parentElement;
             iEl.innerHTML = "";
             let nEl = document.createElement("iframe");
-            nEl.setAttribute("class", "Videos FlexItem");
+            nEl.setAttribute("class", "Video FlexItem");
             nEl.src = "https://www.youtube.com/embed/" + iEl.getAttribute("data-YouTubeVideoID") + "?autoplay=1";
             nEl.setAttribute("allow", "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture");
             nEl.setAttribute("allowfullscreen", "");
